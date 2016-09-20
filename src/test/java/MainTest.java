321aea1d450ad940eb3f0a415f2e050c54406301
starodubtsev_mv <smv@mmk.ru>
2016-09-20 18:12:25 +0500

    1

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,19 +1,10 @@
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.List;
 
 /**
  * Created by smv on 19/09/16.
@@ -21,9 +12,8 @@ import java.util.regex.Pattern;
 public class MainTest {
 
     @Test
-    public void run() {
+    public void run() throws Exception {
+
     }
-
-
 
 }
